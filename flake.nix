ec3f4051d6634825216043bfe0c3174144dd7625
Jorysniel Mata <matajorysniel@gmail.com>
2026-09-09 00:24:32 -0400

    fix: add correct nixosConfigurations module

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = {self, nixpkgs, ...}@inputs: {
-    nixosConfiguration = {
+    nixosConfigurations = {
     nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
