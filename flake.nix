21e94d8d3951fbebbd0f3bd4beb8c48fece2eb03
Jorysniel Mata <matajorysniel@gmail.com>
2026-09-12 19:07:58 -0400

    feat: add first home manager config

--- flake.nix
+++ flake.nix
@@ -3,19 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-  };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {self, nixpkgs, ...}@inputs: {
-    nixosConfigurations = {
-    nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-	./configuration.nix
-
-      ];
     };
-
-};
   };
 
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+	    {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.jorys = import ./home.nix;
+
+	    }
+        ];
+      };
+    };
+  };
 }
---
+++ home.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ...}:
+
+{
+  home.username = "jorys";
+  home.homeDirectory = "/home/jorys";
+
+  home.stateVersion = "26.05";
+}
