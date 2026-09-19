1535a27fd6238afa585cbeac86354fe061ea1e42
Jorysniel Mata Nunez <matajorysniel@gmail.com>
2026-09-18 21:37:54 -0400

    chore: add keybinds for screenshots

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -244,6 +244,8 @@ local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 hl.bind(mainMod .. " + T", hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + Print", hl.dsp.exec_cmd('grim -g "$(slurp)"'))
+hl.bind("Print", hl.dsp.exec_cmd("grim"))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
