287bc138a929031734b126eedc2def7191d62573
Jorysniel Mata Nunez <matajorysniel@gmail.com>
2026-09-21 17:53:33 -0400

    feat: add basic stylix config

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
-  description = "My nix configuration";
+  description = "nix configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:nix-community/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -13,6 +17,7 @@
     self,
     nixpkgs,
     home-manager,
+    stylix,
     ...
   } @ inputs: {
     nixosConfigurations = {
@@ -20,6 +25,7 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          stylix.nixosModules.stylix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
---
+++ stylix.nix
@@ -0,0 +1,6 @@
+{pkgs, ...}: {
+  stylix = {
+    enable = true;
+    polarity = "dark";
+  };
+}
